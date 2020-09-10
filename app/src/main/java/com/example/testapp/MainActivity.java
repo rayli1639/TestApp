020df6b3d8cd2a604ed16d195cca913df11009f8
rayli1639 <ray.z.li@gmail.com>
2020-09-10 01:49:22 -0400

    Added the calculator to fragments

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -2,9 +2,6 @@ package com.example.testapp;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
@@ -26,19 +23,6 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
         FloatingActionButton fab = findViewById(R.id.fab);
-        Button addButton = findViewById(R.id.addButton);
-        addButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EditText num1 = findViewById(R.id.EditText);
-                EditText num2 = findViewById(R.id.EditText2);
-                TextView result = findViewById(R.id.resultTextView);
-                int n1 = Integer.parseInt(num1.getText().toString());
-                int n2 = Integer.parseInt(num2.getText().toString());
-                int r = n1 + n2;
-                result.setText(r + "");
-            }
-        });
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/testapp/ui/main/PlaceholderFragment.java
+++ app/src/main/java/com/example/testapp/ui/main/PlaceholderFragment.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -47,6 +49,19 @@ public class PlaceholderFragment extends Fragment {
             @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
+        Button addButton = root.findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText num1 = getActivity().findViewById(R.id.EditText);
+                EditText num2 = getActivity().findViewById(R.id.EditText2);
+                TextView result = getActivity().findViewById(R.id.resultTextView);
+                int n1 = Integer.parseInt(num1.getText().toString());
+                int n2 = Integer.parseInt(num2.getText().toString());
+                int r = n1 + n2;
+                result.setText(r + "");
+            }
+        });
         final TextView textView = root.findViewById(R.id.section_label);
         pageViewModel.getText().observe(this, new Observer<String>() {
             @Override
